eec921c9c9bc01fc4e22e30ad549015bea0f0860
KH <kkh96071@gmail.com>
2023-04-19 15:24:03 +0900

    Initial commit Hello World~!

---
+++ src/test/java/KKH/example/first_CRUD/FirstCrudApplicationTests.java
@@ -0,0 +1,13 @@
+package KKH.example.first_CRUD;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FirstCrudApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
